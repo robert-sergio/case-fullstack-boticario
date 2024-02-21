6b66c86bf92c9e55a414df8d9fddb6bdc0117583
Robert <robert.sergio.eng@gmail.com>
2024-02-21 07:16:22 -0300

    feat: almost done

--- frontend/src/components/login.jsx
+++ frontend/src/components/login.jsx
@@ -8,6 +8,7 @@ import { useRouter } from 'next/navigation'
 export default function Login() {
   const [email, setEmail] = useState('')
   const [pass, setPass] = useState('')
+  const [error, setError] = useState(false)
   const { setClientData } = useContext(LoginContext)
   const router = useRouter()
 
@@ -15,17 +16,25 @@ export default function Login() {
     event.preventDefault()
     const service = new UserService()
     const response = await service.loginRequest(email, pass)
-    if (response.data.message ==='"Email or Password does not match"'){
-      // colocar mensagem de erro
-      return
+    if (response.data.message ==='Email or Password does not match'){
+      setError(true)
+    } else{
+      setError(false)
+      setClientData(response.data)
+      router.push('/')
     }
-    setClientData(response.data)
-    router.push('/')
   }
 
   return (
-    <>
-        <form onSubmit={handleLogin} className='flex flex-col gap-8 justify-center items-center p-8 bg-green-700 rounded-md'>
+    <div className='w-full flex flex-col items-center justify-center gap-8'>
+        {
+          error?
+          <div className='border rounded-md bg-red-300 text-red-900 p-4'>Email or Password does not match</div>
+          :
+          <></>
+        }
+
+        <form onSubmit={handleLogin} className='flex flex-col w-1/4 h-2/4 gap-8 justify-center items-center p-8 bg-green-700 rounded-md'>
             <h1 className='text-white text-xl'>Faça Login</h1>
 
             <input 
@@ -44,6 +53,6 @@ export default function Login() {
             
             <button type='submit' className='p-2 bg-green-300 rounded-md w-32'>Login</button>
         </form>
-    </>
+    </div>
   )
 }
